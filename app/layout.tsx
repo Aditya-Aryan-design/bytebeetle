7c484f11a0106f8b3877033bf4d4ace20978f4c9
Aditya-Aryan-design <adityaaryan987654321@gmail.com>
2024-05-10 02:09:39 +0530

    Added console

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,13 @@ import Footer from "@/components/Footer";
 import "./globals.css";
 import CallButton from "@/components/CallButton";
 import { memo } from "react";
+import Head from "next/head";
 
 
 
 
 export const metadata: Metadata = {
-  title: "Affordable Web Development Solutions in Pune, Mumbai, and Across India| BlazeSites",
+  title: "Affordable web Development Solutions in Pune, Mumbai, and Across India| BlazeSites",
   description: "BlazeSites offers professional and affordable web development services tailored to businesses in Pune, Mumbai, and throughout India. From custom website design to e-commerce solutions and responsive development, we specialize in delivering high-quality websites that drive results without breaking the bank. Contact us today to elevate your online presence and stand out in the digital marketplace.",
   keywords:[
     "Cheap website developer near me", "Website developer in Pune", "Website developer contact number", "Website developer in Mumbai", "Website development in India", "Web developer in India", "Top 10 website development in India", "How much should I pay a web developer", "How much do Indian web developers cost"
@@ -23,6 +24,9 @@ export default memo(function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="google-site-verification" content="xjTePmVNTd8UlrknIVNILhbThqaZvOpfgjVMu5lJIeI" />
+      </Head>
       <body className="overflow-x-hidden overflow-y-auto select-none flex flex-col">
         <Header />
         <CallButton />
